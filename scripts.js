ab61d1659adb1231f7f6d76ab0b507be8cae3d4b
Robert Graupner <robertgraupner@MacBook-Air-Robert.local>
2017-05-28 20:26:51 +0200

    poprawione

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-var a;
-var h;
-var triangleAre = a*h/2;
-
 a = 2;
 h = 3;
+
+var a;
+var h;
+var triangleArea = a*h/2;
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
